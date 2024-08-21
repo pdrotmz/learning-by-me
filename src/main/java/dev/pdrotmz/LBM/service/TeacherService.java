b409704be88892a13bff77e55208df01bc406d9a
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-20 22:25:57 -0300

    feat(service): add TeacherService class

---
+++ src/main/java/dev/pdrotmz/LBM/service/TeacherService.java
@@ -0,0 +1,48 @@
+package dev.pdrotmz.LBM.service;
+
+import dev.pdrotmz.LBM.model.TeacherModel;
+import dev.pdrotmz.LBM.repository.TeacherRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TeacherService {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    // Teacher register function
+    public TeacherModel registerTeacher(TeacherModel teacherModel) {
+        return teacherRepository.save(teacherModel);
+    }
+
+    // List Teacher function
+    public List<TeacherModel> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+    // List ID Teacher function
+    public Optional<TeacherModel> getTeacherById(UUID idTeacher) {
+        return teacherRepository.findById(idTeacher);
+    }
+
+    // Update Info Teacher function
+    public TeacherModel updateTeacherInfo(UUID idTeacher, TeacherModel updateTeacherInfo) {
+        return teacherRepository.findById(idTeacher).map(TeacherModel -> {
+            TeacherModel.setTeacherName(updateTeacherInfo.getTeacherName());
+            TeacherModel.setTeacherEmail(updateTeacherInfo.getTeacherEmail());
+            TeacherModel.setTeacherPassword(updateTeacherInfo.getTeacherPassword());
+            return teacherRepository.save(TeacherModel);
+        }).orElseThrow(() -> new EntityNotFoundException("Teacher was not found"));
+    }
+
+    // Delete Teacher function
+    public void deleteTeacher(UUID idTeacher) {
+        teacherRepository.deleteById(idTeacher);
+    }
+}
