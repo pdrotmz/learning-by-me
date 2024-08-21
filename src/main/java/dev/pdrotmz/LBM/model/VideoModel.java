e3d04d2c62584a909586ac853a6bb1ff37e41e73
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-21 17:01:37 -0300

    feat(model): add VideoModel class

---
+++ src/main/java/dev/pdrotmz/LBM/model/VideoModel.java
@@ -0,0 +1,25 @@
+package dev.pdrotmz.LBM.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_video_aulas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoModel {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private UUID IdVideo;
+    private String title;
+    private String description;
+    private String video;
+}
