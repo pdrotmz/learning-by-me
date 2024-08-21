54f4deb2bcb35ec8ba557aa7b04a96d459d76990
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-20 21:27:09 -0300

    feat(model): add StudentModel class

---
+++ src/main/java/dev/pdrotmz/LBM/model/StudentModel.java
@@ -0,0 +1,24 @@
+package dev.pdrotmz.LBM.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idStudent;
+    private String studentUsername;
+    private String studentPassword;
+}
