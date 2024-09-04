d7ae7b0566a7c2f059d24588be4cff0afb824ade
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-04 09:58:02 -0300

    feat: add function to find teachers by their email

--- src/main/java/dev/pdrotmz/LBM/repository/TeacherRepository.java
+++ src/main/java/dev/pdrotmz/LBM/repository/TeacherRepository.java
@@ -3,7 +3,10 @@ package dev.pdrotmz.LBM.repository;
 import dev.pdrotmz.LBM.domain.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+
+    Optional<Teacher> findByTeacherEmail(String teacherEmail);
 }
