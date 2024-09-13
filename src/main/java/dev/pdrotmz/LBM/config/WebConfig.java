e1b93636e0e5f2dd85d66813cd39cae8897b3e67
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-13 16:31:11 -0300

    feat: add WebConfig

---
+++ src/main/java/dev/pdrotmz/LBM/config/WebConfig.java
@@ -0,0 +1,22 @@
+package dev.pdrotmz.LBM.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
+
+
+
--- src/main/java/dev/pdrotmz/LBM/domain/controller/AuthTeacherController.java
+++ src/main/java/dev/pdrotmz/LBM/domain/controller/AuthTeacherController.java
@@ -9,16 +9,14 @@ import dev.pdrotmz.LBM.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth-teacher")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthTeacherController {
 
     private final TeacherRepository teacherRepository;
